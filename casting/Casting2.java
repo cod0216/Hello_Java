9a7a813b0bf69dd88b1ad2855f27c2cdc769ced3
cod0216 <cod0216@nate.com>
2023-12-04 18:55:05 +0900

    Today I Learned Scope, Casting about Java

---
+++ casting/Casting2.java
@@ -0,0 +1,15 @@
+package casting;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Casting2 {
+    public static void main(String[] args) {
+        double doubleValue = 1.5;
+        int intValue = 0;
+
+        // intValue = doubleValue; //컴파일 오류 발생
+
+        intValue = (int) doubleValue; // 형변환
+        System.out.println(intValue);
+    }
+}
