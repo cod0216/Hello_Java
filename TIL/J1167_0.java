a140e410dc2ec6ce04ab3ff000fccf579ddc94f9
cod0216 <cod0216@nate.com>
2024-04-24 23:51:48 +0900

    PS J1167_0 J1920_0

--- TIL/J1167_0.java
+++ TIL/J1167_0.java
@@ -3,44 +3,65 @@ package TIL;
 import java.util.*;
 import java.io.*;
 
-public class J1167_0 {
-    static LinkedList<int[]>[] A;
-    static Boolean[] visited;
+    public class J1167_0 {
+        static LinkedList<int[]>[] A;
+        static boolean[] visited;
+        static int maxNode;
+        static int maxNum;
+        static int[] distance;
+        public static void main(String[] args) throws IOException {
+            Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args)throws IOException{
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int Num = Integer.parseInt(bufferedReader.readLine());
+            int num = scanner.nextInt();
+            A = new LinkedList[num+1];
+            distance = new int[num+1];
+            visited = new boolean[num+1];
 
-        visited = new Boolean[Num +1];
-        A = new LinkedList[Num+1];
-
-        for(int i =0; i<Num+1; i++){
-            A[i] = new LinkedList<int[]>();
-        }
-
-        for(int i = 0; i<Num; i++) {
-            String[] input = bufferedReader.readLine().split(" ");
-            int node = Integer.parseInt(input[0]);
-            int cnt = 1;
-            while(Integer.parseInt(input[cnt]) != -1) {
-                int edge = Integer.parseInt(input[cnt]);
-                cnt++;
-                int edgeLength = Integer.parseInt(input[cnt]);
-                cnt++;
-                A[node].add(new int[]{edge, edgeLength});
+            for(int i = 0 ; i < num+1; i++){
+                A[i] = new LinkedList<>();
             }
+            for(int i = 1; i <= num; i++){
+                int node = scanner.nextInt();
+                int edge = scanner.nextInt();
+                while(edge != -1){
+
+                    int len = scanner.nextInt();
+                    A[node].add(new int[]{edge,len});
+                    edge = scanner.nextInt();
+                }
+            }
+
+            BFS(1, 0 );
+            visited = new boolean[num+1];
+            distance = new int[num+1];
+            BFS(maxNode, distance[maxNode]);
+
+            System.out.println(maxNum);
+
+        }
+        private static void BFS(int node, int len){
+            visited[node] = true;
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(node);
+            distance[node] = len;
+            while(!queue.isEmpty()){
+                node = queue.poll();
+
+                for(int[] i : A[node]){
+                    if(!visited[i[0]]){
+                        visited[i[0]] = true;
+                        distance[i[0]] = distance[node]+i[1];
+                        if(maxNum < distance[i[0]]){
+                            maxNum = distance[i[0]];
+                            maxNode = i[0];
+                        }
+                        queue.add(i[0]);
+
+                    }
+                }
+            }
+
+
         }
 
     }
-//    private static void BFS(int node){
-//
-//        Queue<int[]> queue = new LinkedList<int[]>();
-//        visited[node] = true;
-//        queue.add(A[node]);
-//
-//        while(!queue.isEmpty()){
-//            int value = queue.poll();
-//
-//        }
-//    }
-}
---
+++ TIL/J1920_0.java
@@ -0,0 +1,52 @@
+package TIL;
+import java.util.*;
+import java.io.*;
+public class J1920_0 {
+
+    public static void main(String[] args) throws IOException{
+
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+
+        int Num = Integer.parseInt(buffer.readLine());
+        String[] input = buffer.readLine().split(" ");
+
+        int[] arr1 = new int[Num];
+        for(int i = 0; i < Num; i++){
+            arr1[i] = Integer.parseInt(input[i]);
+        }
+        int num = Integer.parseInt(buffer.readLine());
+        input = buffer.readLine().split(" ");
+        int[] arr2 = new int[num];
+        for(int i = 0; i< num; i++){
+            arr2[i] = Integer.parseInt(input[i]);
+        }
+
+        Arrays.sort(arr1);
+        for(int i = 0; i <num; i++){
+            int start = 0;
+            int end = Num-1;
+            int key = arr2[i];
+            int answer = 0;
+            while(end >=start){
+                int index = (start + end)/2;
+                if (key < arr1[index]) {
+                    end = index-1;
+                } else if (key> arr1[index]){
+                    start = index+1;
+                }
+                else{
+                    answer =1;
+                    break;
+                }
+
+            }
+            if(answer == 0){
+                System.out.println("0");
+            }
+            if(answer == 1){
+                System.out.println("1");
+            }
+        }
+
+    }
+}
