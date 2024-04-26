e6889015460c07ca1b5d85c7b18205356c13a33a
cod0216 <cod0216@nate.com>
2024-04-26 23:51:06 +0900

    ps review

---
+++ TIL/J1300.java
@@ -0,0 +1,22 @@
+package TIL;
+
+import java.util.*;
+import java.io.*;
+
+public class J1300 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        int index = Integer.parseInt(buffer.readLine());
+        int key = Integer.parseInt(buffer.readLine());
+        int num=0;
+        int[][] array = new int[index][index];
+        int[] value = new int[index*index];
+        for(int i = 0; i < index; i++){
+            for(int j = 0; j< index; j++){
+                value[] = (i+1)*(j*1);
+                num++;
+            }
+        }
+
+    }
+}
