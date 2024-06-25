02054b2e1d59f4a4d976053082b95798d5ab8fe9
cod0216 <cod0216@nate.com>
2024-06-25 23:19:14 +0900

    Today I Learned Map

---
+++ collection/map/MapMain3.java
@@ -0,0 +1,23 @@
+package collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMain3 {
+    public static void main(String[] args) {
+        Map<String, Integer> studentMap = new HashMap<>();
+
+        studentMap.put("StudentA", 50);
+        System.out.println(studentMap);
+
+        if(!studentMap.containsKey("StudentA")){
+            studentMap.put("StudentA", 100);
+        }
+        System.out.println(studentMap);
+
+        studentMap.putIfAbsent("StudentA", 100);
+        studentMap.putIfAbsent("StudentB", 100);
+        System.out.println(studentMap);
+        
+    }
+}
