917b4b79ca100fd287699a89f50d2576a362aeb1
cod0216 <cod0216@nate.com>
2024-07-09 20:39:19 +0900

    Today I Learned Iteralbe

--- collection/iterable/MyArray.java
+++ collection/iterable/MyArray.java
@@ -3,14 +3,14 @@ package collection.iterable;
 import java.util.Iterator;
 
 public class MyArray implements Iterable<Integer> {
+    private int[] number;
 
-    private int[] numbers;
-
-    public MyArray(int[] numbers) {
-        this.numbers = numbers;
+    public MyArray(int[] number) {
+        this.number = number;
     }
+
     @Override
     public Iterator<Integer> iterator() {
-        return new MyArrayIterator(numbers);
+        return new MyArrayIterator(number);
     }
 }
--- collection/iterable/MyArrayIterator.java
+++ collection/iterable/MyArrayIterator.java
@@ -1,20 +1,18 @@
 package collection.iterable;
 
 import java.util.Iterator;
-import java.util.function.Consumer;
 
 public class MyArrayIterator implements Iterator<Integer> {
-
     private int currentIndex = -1;
     private int[] targetArr;
 
-    public MyArrayIterator(int[] targetArr) {
+    public MyArrayIterator(int[] targetArr){
         this.targetArr = targetArr;
     }
 
     @Override
     public boolean hasNext() {
-        return currentIndex < targetArr.length -1;
+        return currentIndex < targetArr.length-1;
     }
 
     @Override
