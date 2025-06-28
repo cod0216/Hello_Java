7aa219525a01a638624dd432fcf4201cf105d520
cod0216 <cod0216@nate.com>
2025-06-28 23:33:46 +0900

    Today I Learned Thread

--- thread/bounded/BoundedMain.java
+++ thread/bounded/BoundedMain.java
@@ -10,9 +10,9 @@ public class BoundedMain {
     public static void main(String[] args) {
 
         // 1. BoundedQueue 선택
-        BoundedQueue queue = new BoundedQueueV1(2);
+        BoundedQueue queue = new BoundedQueueV2(2);
 
-        // 2. 생산자, 소비자 실행 순서 선택, 반드 하나만 선탹!
+        // 2. 생산자, 소비자 실행 순서 선택, 반드 하나만 선택!
 //        producerFirst(queue);
         consumerFirst(queue);
     }
