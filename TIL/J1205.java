c336095ef66260254b17a159461d23fc27991f3a
cod0216 <cod0216@nate.com>
2025-07-24 18:48:27 +0900

    ps

--- TIL/J1205.java
+++ TIL/J1205.java
@@ -3,6 +3,10 @@ package TIL;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /*
     디제이맥스 게임 노래 --> 랭킹 리스트 존재
@@ -20,11 +24,39 @@ import java.io.InputStreamReader;
 
     N, 태수, P
  */
+
+/*
+    10 1 10
+    10 9 8 7 6 5 4 3 2 1
+ */
 public class J1205 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
 
+        String[] input = buffer.readLine().split(" ");
 
+        int N, S, P;
+        N = Integer.parseInt(input[0]);
+        S = Integer.parseInt(input[1]);
+        P = Integer.parseInt(input[2]);
+
+        Integer[] arr = new Integer[N];
+        input = buffer.readLine().split(" ");
+
+        for (int i = 0; i <N; i++) {
+           arr[i] = Integer.parseInt(input[i]);
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        if(N==P && S <= arr[N-1]){
+            System.out.println(-1);
+        }else{
+            int rank = 1;
+            for (int i = 0; i<N; i++){
+                if(S < arr[i]) rank++;
+                else break;
+            }
+            System.out.println(rank);
+        }
     }
 }
