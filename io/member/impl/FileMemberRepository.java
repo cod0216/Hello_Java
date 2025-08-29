9614d86dba0be0f84b036836318651afe8f5e361
cod0216 <cod0216@nate.com>
2025-08-29 16:17:16 +0900

    IO

--- io/member/impl/FileMemberRepository.java
+++ io/member/impl/FileMemberRepository.java
@@ -3,10 +3,9 @@ package io.member.impl;
 import io.member.Member;
 import io.member.MemberRepository;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.nio.charset.StandardCharsets.*;
@@ -18,12 +17,29 @@ public class FileMemberRepository implements MemberRepository {
 
     @Override
     public void add(Member member) {
-        FileWriter fw = new FileWriter(FILE_PATH, UTF_8, true);
-        BufferedWriter bw = new BufferedWriter(fw);
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH, UTF_8, true))){
+            bw.write(member.getId() + DELIMETER + member.getName() + DELIMETER + member.getAge());
+            bw.newLine();
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public List<Member> findAll() {
-        return null;
+        List<Member> members = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(FILE_PATH,UTF_8))){
+            String line;
+            while((line = br.readLine()) != null){
+                String[] memberData = line.split(DELIMETER);
+                members.add(new Member(memberData[0], memberData[1], Integer.parseInt(memberData[2])));
+            }
+            return members;
+        } catch(FileNotFoundException e) {
+            return new ArrayList<>();
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
     }
 }
