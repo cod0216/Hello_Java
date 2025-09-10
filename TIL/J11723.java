b11369e7eaac2e984510fc190ed4b501812f4b7e
cod0216 <cod0216@nate.com>
2025-09-10 18:05:10 +0900

    ps

---
+++ TIL/J11723.java
@@ -0,0 +1,48 @@
+package TIL;
+import java.io.*;
+import java.util.*;
+
+public class J11723 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(buffer.readLine());
+        int t = 0;
+        int n = 0;
+
+
+        while(T > t){
+            String[] input = buffer.readLine().split(" ");
+            String fun = input[0];
+            int num = 0;
+            if(!fun.equals("all") && !fun.equals("empty")) num = Integer.parseInt(input[1]);
+            switch(fun){
+                case "add" :
+                    n |= 1<<(num-1);
+                    break;
+                case "remove" :
+                    num = ~(1<<(num-1));
+                    n &= num;
+                    break;
+                case "check" :
+                    if((n & (1<<(num-1))) != 0) sb.append("1");
+                    else sb.append("0");
+                    sb.append("\n");
+                    break;
+                case "all" :
+                    n = (1<<20) -1;
+                    break;
+                case "toggle" :
+                    n ^= (1<<num-1);
+                    break;
+                default :
+                    n = 0;
+                    break;
+            }
+
+            t++;
+        }
+        System.out.println(sb.toString());
+
+    }
+}
