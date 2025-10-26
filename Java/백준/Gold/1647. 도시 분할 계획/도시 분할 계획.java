42fc9e6c482cea065afa19fe2161aac711155267
EunChang <83526046+cod0216@users.noreply.github.com>
2025-10-26 11:51:56 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1552 ms, Memory: 325012 KB -BaekjoonHub

--- Java/백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ Java/백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,54 +1,54 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static int parents[];
+    static int A[], M, N;
     public static int find(int n){
-        if(parents[n] != n) return parents[n] = find(parents[n]);
-        return parents[n];
+        if(A[n] != n) return A[n] = find(A[n]);
+        return A[n];
     }
-
+    
     public static void union(int a, int b){
-        int max = Math.max(find(a), find(b));
-        int min = Math.min(find(a), find(b));
-
-        if(min != max) parents[max] = min;
+        a = find(a);
+        b = find(b);
+        
+        int max = Math.max(a,b);
+        int min = Math.min(a,b);
+        
+        A[max] = min;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
         String[] input = buffer.readLine().split(" ");
-
-        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> a[2]-b[2]);
-
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
-
-        parents = new int[N+1];
-
-        for (int i = 1; i <= N; i++) {
-            parents[i] = i;
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        A = new int[N+1];
+        
+        for(int i = 1; i <= N; i++){
+            A[i] = i;
         }
-
-        for (int i = 0; i < M; i++) {
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
+        
+        for(int i = 0; i < M; i++){
             input = buffer.readLine().split(" ");
-            int start = Integer.parseInt(input[0]);
-            int end = Integer.parseInt(input[1]);
-            int weight = Integer.parseInt(input[2]);
-
-            queue.offer(new int[]{start, end, weight});
+            int s = Integer.parseInt(input[0]);
+            int e = Integer.parseInt(input[1]);
+            int w = Integer.parseInt(input[2]);
+            
+            pq.offer(new int[]{s,e,w});
         }
         int ans = 0;
         int max = 0;
-        while(!queue.isEmpty()){
-            int[] e = queue.poll();
-
-            if(find(e[0]) != find(e[1])){
-                ans += e[2];
-                max = Math.max(e[2], max);
-                union(e[0], e[1]);
+        while(!pq.isEmpty()){
+            int[] cur = pq.poll();
+            int s = cur[0]; int e = cur[1]; int w = cur[2];
+            if(find(s) != find(e)){
+                union(s, e);
+                ans +=w;
+                max = Math.max(w, max);
             }
         }
-
         System.out.println(ans-max);
     }
 }
