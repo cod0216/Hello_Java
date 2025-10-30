84804476b1fe46771a729e9417fe43be24819f42
EunChang <83526046+cod0216@users.noreply.github.com>
2025-10-30 14:59:38 +0900

    [Gold II] Title: 나는 가르친다 스위핑을, Time: 604 ms, Memory: 66024 KB -BaekjoonHub

---
+++ Java/백준/Gold/14266. 나는 가르친다 스위핑을/나는 가르친다 스위핑을.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+class Info implements Comparable<Info> {
+    double e;
+    int v;
+    
+    public Info(double e, int v){
+        this.e = e;
+        this.v = v;
+    }
+    
+    public int compareTo(Info o){
+        if(o.e == this.e) return  o.v - this.v ;
+        return Double.compare(this.e, o.e);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(buffer.readLine());
+        
+        PriorityQueue<Info> queue = new PriorityQueue<>();
+
+        for(int i = 0 ; i < N; i++){
+            String[] input = buffer.readLine().split(" ");
+            
+            int x1 = Integer.parseInt(input[0]);
+            int y1 = Integer.parseInt(input[1]);
+            
+            int x2 = Integer.parseInt(input[2]);
+            int y2 = Integer.parseInt(input[3]);
+            
+            double res1 = getLine(x1,y1);
+            double res2 = getLine(x2,y2);
+            
+            double max = Math.max(res1, res2);
+            double min = Math.min(res1, res2);
+            
+            queue.offer(new Info(max, -1));
+            queue.offer(new Info(min, 1));
+        }
+        
+        int max = 0;
+        int cnt = 0;
+        
+        while(!queue.isEmpty()){
+            cnt += queue.poll().v;
+            
+            max = Math.max(cnt,max);
+        }
+        
+        System.out.println(max);
+    }
+    
+    public static double getLine(int x, int y){
+        return (double)y/x;
+    }
+}
