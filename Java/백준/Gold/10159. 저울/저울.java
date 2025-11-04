783d4728cce575060e278e2cf03acd1772465327
EunChang <83526046+cod0216@users.noreply.github.com>
2025-11-04 12:45:20 +0900

    [Gold IV] Title: 저울, Time: 116 ms, Memory: 14892 KB -BaekjoonHub

--- Java/백준/Gold/10159. 저울/저울.java
+++ Java/백준/Gold/10159. 저울/저울.java
@@ -1,41 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(buffer.readLine());
-        int m = Integer.parseInt(buffer.readLine());
-
-        boolean arr[][] = new boolean[n+1][n+1];
-            for(int j = 0; j<m; j++){
-               String[] input = buffer.readLine().split(" ");
-               int a = Integer.parseInt(input[0]);
-               int b = Integer.parseInt(input[1]);
-
-               arr[a][b] = true;
-            }
-
-        for(int k = 1; k <= n; k++){
-            for(int i = 1; i <= n; i++){
-                for(int j = 1; j <= n; j++){
-                    if(arr[i][k] && arr[k][j])
+        int N = Integer.parseInt(buffer.readLine());
+        int M = Integer.parseInt(buffer.readLine());
+        
+        boolean[][] arr = new boolean[N+1][N+1];
+        
+        for(int i = 0 ; i < M; i++){
+            String[] input = buffer.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+            
+            arr[a][b] = true;
+        }
+        
+        for(int k = 1 ; k <= N; k++){
+            for(int i = 1 ; i <= N; i++){
+                for(int j = 1 ; j <= N; j++){
+                    if(arr[i][k] && arr[k][j]){
                         arr[i][j] = true;
+                    }
                 }
             }
         }
-
-        int cnt =0;
-        for(int i =1; i<=n; i++){
-            cnt = 0;
-            for(int j =1; j<=n; j++){
-                if (i != j && (arr[i][j] || arr[j][i])) {
-                    cnt++;
-                }
+        
+        for(int i = 1 ; i <= N; i++){
+            int cnt = 0;
+            for(int j = 1 ; j <= N; j++){
+                if(i != j && (arr[i][j] || arr[j][i]))cnt++;
             }
-            System.out.println(n-1-cnt);
+            System.out.println(N-1-cnt);
         }
     }
 }
