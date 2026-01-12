7781f90e9d3bcf2607355154b8f7670462be5af6
EunChang <83526046+cod0216@users.noreply.github.com>
2026-01-12 21:30:35 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14284 KB -BaekjoonHub

---
+++ Java/백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(buffer.readLine());
+        int[] time = new int[N+1];
+        int[] money = new int[N+1];        
+
+        for(int i = 0 ; i < N; i++){
+            String[] input = buffer.readLine().split(" ");
+            time[i+1]= Integer.parseInt(input[0]);
+            money[i+1] = Integer.parseInt(input[1]);
+        }
+        
+        int[] dp = new int[N+2];
+
+
+        for(int i = N; i >= 1; i--){
+            dp[i] = dp[i+1];
+            
+            int next = i + time[i];
+            if(next <= N+1){
+                dp[i] = Math.max(dp[i], money[i] + dp[next]);
+            }
+        }
+
+        System.out.println(dp[1]);
+    }
+}
