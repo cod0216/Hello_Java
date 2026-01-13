c487c1b4affbba80e663c90b3fcd1e12839af71d
EunChang <83526046+cod0216@users.noreply.github.com>
2026-01-13 21:29:50 +0900

    [Silver V] Title: 집합, Time: 1196 ms, Memory: 327656 KB -BaekjoonHub

--- Java/백준/Silver/11723. 집합/집합.java
+++ Java/백준/Silver/11723. 집합/집합.java
@@ -1,44 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(buffer.readLine());
+        int bit = 0;
+        int t =0;
         StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(buffer.readLine());
-        int t = 0;
-        int n = 0;
-        
-        
-        while(T > t){
+        while(N>t){
             String[] input = buffer.readLine().split(" ");
-            String fun = input[0];
-            int num = 0;
-            if(!fun.equals("all") && !fun.equals("empty")) num = Integer.parseInt(input[1]);
-            switch(fun){
+            String keyword = input[0];
+            int v = 0;
+            if(!keyword.equals("all")  && !keyword.equals("empty")) v = Integer.parseInt(input[1]);
+            
+            switch(keyword){
                 case "add" :
-                    n |= 1<<(num-1);
+                    bit |= (1<<v);
+                    break;
+                case "check" :
+                    if((bit & (1<<v)) == 0) sb.append(0).append("\n");
+                    else sb.append(1).append("\n");
                     break;
                 case "remove" :
-                    num = ~(1<<(num-1));
-                    n &= num;
-                    break;    
-                case "check" :
-                    if((n & (1<<(num-1))) != 0) sb.append("1");
-                    else sb.append("0");
-                    sb.append("\n");
-                    break;
-                case "all" :
-                    n = (1<<20) -1;
+                    bit &= ~(1<<v);
                     break;
                 case "toggle" :
-                    n ^= (1<<num-1);
+                    bit ^=(1<<v);
+                    break;    
+                case "all" :
+                    bit =(1<<21)-1;
                     break;
                 default :
-                    n = 0;    
-                    break;
+                    bit = 0;
             }
-            
             t++;
         }
         System.out.println(sb.toString());
