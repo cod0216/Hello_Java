2184ec0f7fb085150d63dd87a3b4b7635aa5467f
EunChang <83526046+cod0216@users.noreply.github.com>
2026-01-24 22:54:06 +0900

    [Bronze I] Title: 싸이클, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2526. 싸이클/싸이클.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = buffer.readLine().split(" ");
+		
+		int N = Integer.parseInt(input[0]);
+		int M = Integer.parseInt(input[1]);
+		boolean flag = true;
+		
+		HashMap<Integer,Integer> map = new HashMap<>();
+		int cnt = 1;
+		int ans = 0;
+		map.put(N,cnt);
+		int last = N;
+		while(flag) {
+			cnt++;
+			last = last*N%M;
+			if(last != N && map.containsKey(last)) {
+				flag = !flag;	
+				int order = map.get(last);
+				ans = cnt-order;
+			}
+			else map.put(last,cnt);
+			
+		}
+		System.out.print(ans);
+	}
+}
