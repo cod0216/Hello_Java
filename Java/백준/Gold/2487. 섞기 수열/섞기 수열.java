03967cc4695300a556965d62f4e2162c1849fd26
EunChang <83526046+cod0216@users.noreply.github.com>
2026-01-31 20:20:25 +0900

    [Gold IV] Title: 섞기 수열, Time: 136 ms, Memory: 17056 KB -BaekjoonHub

---
+++ Java/백준/Gold/2487. 섞기 수열/섞기 수열.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(buffer.readLine());
+        String[] input = buffer.readLine().split(" ");
+        int[] arr = new int[N+1];
+        
+        for(int i = 1 ; i <= N; i++){
+            arr[i] = Integer.parseInt(input[i-1]);
+        }
+        
+        boolean[] visited = new boolean[N+1];
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 1; i <= N; i++){
+            if(visited[i]) continue;
+
+            visited[i] = true;
+            int now = arr[i];
+            int cnt = 1;
+            
+            while(now != i){
+                visited[now] = true;
+                cnt++;
+                now = arr[now];
+
+            }
+            set.add(cnt);
+        }
+        
+        Iterator<Integer> iter = set.iterator();
+        long result = 1;
+        
+        while(iter.hasNext()){
+            result = lcm(result, (long)iter.next());
+        }
+        System.out.println(result);
+    }
+    
+    public static int lcm(long a, long b){
+        long gcd;
+        if(a > b) gcd = gcd(a, b);
+        else gcd = gcd(b, a);
+        
+        return (int)((a*b)/gcd);
+    }
+    
+    public static long gcd(long a, long b){
+        if(b == 0) return a;
+        return gcd(b, a%b);
+    }
+}
