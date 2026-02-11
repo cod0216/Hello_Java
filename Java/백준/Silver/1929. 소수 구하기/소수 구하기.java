65e49a607fe80a920bd8a71b4c21653383d8e84e
EunChang <83526046+cod0216@users.noreply.github.com>
2026-02-12 08:35:32 +0900

    [Silver III] Title: 소수 구하기, Time: 800 ms, Memory: 40956 KB -BaekjoonHub

--- Java/백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ Java/백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -2,24 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static int N, M;
     public static void main(String[] args) throws IOException{
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
         String[] input = buffer.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int M = Integer.parseInt(input[1]);
         
-        boolean[] prime = new boolean[M+1];
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        
+        int[] arr = new int[M+1];
+        
+        Arrays.fill(arr, 1);
+        arr[1] = 0;
         
         for(int i = 2; i <= M; i++){
-            if(!prime[i]){
-                if(i>=N) System.out.println(i);
-                int idx = i;
-                int n = 2;
-                while(idx <= M){
-                    prime[idx] = true;
-                    idx = i*n++;
-                }
+            if(arr[i] == 0) continue;
+            int cnt = 2;
+            for(int j = i; j <= M; j = i*cnt){
+                if(j == i) continue;
+                arr[j] = 0;
+                cnt++;
             }
+        }
+        
+        for(int i = N ; i <= M; i++){
+            if(arr[i] == 1)
+            System.out.println(i);
         }
     }
 }
