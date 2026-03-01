3bce3b6166ba0255c4208cdd9f0c0064d04d9f06
EunChang <83526046+cod0216@users.noreply.github.com>
2026-03-01 20:12:10 +0900

    [Silver II] Title: 오락실에 간 총총이, Time: 872 ms, Memory: 180428 KB -BaekjoonHub

--- Java/백준/Silver/26071. 오락실에 간 총총이/오락실에 간 총총이.java
+++ Java/백준/Silver/26071. 오락실에 간 총총이/오락실에 간 총총이.java
@@ -3,23 +3,23 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(buffer.readLine());
 
         int minR = Integer.MAX_VALUE, maxR = Integer.MIN_VALUE;
         int minC = Integer.MAX_VALUE, maxC = Integer.MIN_VALUE;
         int total = 0;
-
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < N; j++) {
-                char ch = line.charAt(j);
-                if (ch == 'G') {
+        
+        for(int i= 0 ; i < N ; i++){
+            String[] input = buffer.readLine().split("");
+            for(int j= 0; j < N; j++){            
+                char v = input[j].charAt(0);
+                if(v == 'G'){
+                    if(minR > i) minR = i;
+                    if(maxR < i) maxR = i;
+                    if(minC > j) minC = j;
+                    if(maxC < j) maxC = j;
                     total++;
-                    if (i < minR) minR = i;
-                    if (i > maxR) maxR = i;
-                    if (j < minC) minC = j;
-                    if (j > maxC) maxC = j;
                 }
             }
         }
@@ -30,19 +30,18 @@ public class Main {
         }
 
         int ans;
-        if (minR == maxR) {
-            ans = Math.min(maxC, (N - 1) - minC);
-        } else if (minC == maxC) {
-            ans = Math.min(maxR, (N - 1) - minR);
+
+        if(maxR == minR) {
+            ans = Math.min(maxC, (N-1)-minC);
+        } else if (maxC == minC) {
+            ans = Math.min(maxR, (N-1)-minR);
         } else {
             int top = maxR;
-            int bottom = (N - 1) - minR;
+            int bottom = (N-1) - minR;
             int left = maxC;
-            int right = (N - 1) - minC;
-
-            ans = Math.min(
-                    Math.min(top + left, top + right),
-                    Math.min(bottom + left, bottom + right)
+            int right = (N-1) - minC;
+            ans = Math.min( Math.min(top+left, top+right),
+                      Math.min(bottom+left, bottom+right)
             );
         }
 
